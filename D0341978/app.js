b17dda2b2214c14a2b6c10924b1f4180c149187e
fcu-d0341978 <32485367+fcu-d0341978@users.noreply.github.com>
2018-01-16 16:03:03 +0800

    server

--- D0341978/app.js
+++ D0341978/app.js
@@ -3,12 +3,18 @@ var path = require('path');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var fileUpload = require('express-fileupload');
 
 var index = require('./routes/index');
 var store = require('./routes/store');
 var register = require('./routes/register');
 var login = require('./routes/login');
+var intro = require('./routes/intro');
+var Tintro = require('./routes/Tintro');
+var upload = require('./routes/upload');
+
 var app = express();
+var fs = require('fs');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,11 +27,39 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(fileUpload());
+
 
 app.use('/', index);
 app.use('/store', store);
 app.use('/register', register);
 app.use('/login', login);
+app.use('/intro', intro);
+app.use('/Tintro', Tintro);
+app.use('/upload', upload);
+
+
+
+app.get('/', function (req, res){
+    res.sendFile(__dirname + '/upload.ejs');
+});
+
+app.post('/upload', function(req, res) {
+    if (!req.files)
+        return res.status(400).send('No files were uploaded.');
+
+    // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+    let sampleFile = req.files.sampleFile;
+
+    // Use the mv() method to place the file somewhere on your server
+    sampleFile.mv('C:\\Users\\Patrick\\WebstormProjects\\Final Project\\public\\images', function(err) {
+        if (err)
+            return res.status(500).send(err);
+
+        res.send('File uploaded!');
+    });
+});
+
 
 
 console.log("Server is now running...")
